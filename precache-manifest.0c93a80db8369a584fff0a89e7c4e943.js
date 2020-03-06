d96640153c11051644a887c544de9f79b02b5128
adityawankhede5 <adityawankhede555@gmail.com>
2020-03-07 02:13:31 +0530

    Updates

---
+++ precache-manifest.0c93a80db8369a584fff0a89e7c4e943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65649dd924ef60ca94b39528072439c2",
+    "url": "/billsplitter/index.html"
+  },
+  {
+    "revision": "5e227f6937f18c8ff6e3",
+    "url": "/billsplitter/static/css/main.83a034e4.chunk.css"
+  },
+  {
+    "revision": "bbd1182402243ed3d71c",
+    "url": "/billsplitter/static/js/2.0d013df7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/billsplitter/static/js/2.0d013df7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e227f6937f18c8ff6e3",
+    "url": "/billsplitter/static/js/main.eaa93d49.chunk.js"
+  },
+  {
+    "revision": "aa134d13345f18f684c4",
+    "url": "/billsplitter/static/js/runtime-main.4e7b5049.js"
+  }
+]);
